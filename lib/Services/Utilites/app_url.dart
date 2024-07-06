2524c6626170f12410b1c199a0889b6390af94dc
Muhammad Daniyal Khan <mdk968196@gmail.com>
2024-07-06 11:39:37 -0600

    countries

--- lib/Services/Utilites/app_url.dart
+++ lib/Services/Utilites/app_url.dart
@@ -2,5 +2,5 @@ class AppUrl {
   static const String baseUrl = 'https://disease.sh/v3/covid-19/';
 
   static const String worldStatesApi = baseUrl + 'all';
-  static const String CountriesList = baseUrl + 'all';
+  static const String CountriesList = baseUrl + 'countries';
 }
